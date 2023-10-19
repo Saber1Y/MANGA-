a25e498636a475c2521c2d6e5d2f7cc4ad273cc1
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-19 23:07:11 +0100

    CREATE: Created Diffrent comp

---
+++ src/container/diffrenent/Diffrerent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { images, data } from '../../constants'
+
+const Diffrerent = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Diffrerent
