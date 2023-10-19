74a265f9e5f49b838dfc3fa93c384d0a98639680
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-19 11:46:27 +0100

    UPDATE: Header Component Created

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import Subheading from '../../container/subheading/Subheading';
+import Illustration from '../../assets/illustration-intro.svg';
 
 const Heading = () => {
   return (
-    <div>
-      <Subheading
-        title="Bring everyone together to build better products"
-        paragraph="Manga makes it simple for software teams to plan day-to-day tasks while keeping the larger team's goals in view."
-        btn="Get Started"
-      />
-    </div>
+    <>
+      <div className="container flex flex-col-reverse items-center mx-auto  md:space-y-0 md:flex-row">
+        <Subheading
+          title="Bring everyone together to build better products"
+          paragraph="Manga makes it simple for software teams to plan day-to-day tasks while keeping the larger team's goals in view."
+          btn="Get Started"
+        />
+        <img src={Illustration} alt="" /> 
+      </div>
+    </>
   );
 }
 
--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Subheading  from '../../container/subheading/Subheading'
+
 
 const Features = () => {
   return (
