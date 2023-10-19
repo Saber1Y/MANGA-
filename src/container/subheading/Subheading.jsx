a12dec1054dcfeb7b3285db57c3284431a9c838a
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-19 22:19:15 +0100

    UPDATE: Feature re-usable

--- src/container/subheading/Subheading.jsx
+++ src/container/subheading/Subheading.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Subheading = ({ title, paragraph, btn, src }) => {
+const Subheading = ({ title, paragraph, btn }) => {
   return (
-    <div className="flex flex-col mb-32 space-y-12 md:w-1/2">
+  <div className="flex flex-col mb-32 space-y-12 md:w-1/2">
       <h1 className="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left">
         {title}
       </h1>
@@ -10,15 +10,18 @@ const Subheading = ({ title, paragraph, btn, src }) => {
         {paragraph}
       </p>
       <div className="flex justify-center md:justify-start">
-        <a
-          href=""
-          className="p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight"
-        >
-          {btn}
-        </a>
+      {btn && (
+        <div className="flex justify-center md:justify-start">
+          <a
+            href=""
+            className="p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight"
+          >
+            {btn}
+          </a>
       </div>
-      
+      )}
     </div>
+  </div>
   );
 }
 
