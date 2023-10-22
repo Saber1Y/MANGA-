1f980481c69b88c4e80dea04d1bd1c2bfc88870f
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-22 14:10:46 +0100

    UODATE: Correctly mapped around difference comp

--- src/components/Difference/Difference.jsx
+++ src/components/Difference/Difference.jsx
@@ -4,34 +4,42 @@ import data from '../../constants/data';
 
 const Difference = () => {
   return (
-<>
-    <div className="max-w-6xl px-5 mx-auto mt-32 text-center">
-      <h2 className="text-4xl font-bold text-center">
-        What's Different about Manga?
-      </h2>
-        <div className="flex flex-col mt-24 md:flex-row md:space-x-6">
+<section id="testimonials">
+  <div className="max-w-6xl px-5 mx-auto mt-32 text-center">
+    <h2 className="text-4xl font-bold text-center">
+      What's Different About Manage?
+    </h2>
+
+    <div className="flex flex-col mt-24 md:flex-row md:space-x-6">
+      {data.peoplesNames.map((person, index) => (
         <div
-            className="flex flex-col items-center p-6 space-y-6 rounded-lg bg-veryLightGray md:w-1/3"
-          >
-            {data.peoplesNames.map((name, index)=> (
-          <Different 
-              key={index}
-              imgUrl={name.img}
-              name={name.name}
-              review={name.review}
-              />
-            ))}
-            </div>
-            <div class="my-16">
-            <a
-            href="#"
-            className="p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight"
-            >Get Started
-            </a>
+          key={index}
+          className="flex flex-col items-center p-6 space-y-6 rounded-lg bg-veryLightGray md:w-1/3"
+        >
+          <img
+            src={person.img}
+            className="w-16 -mt-14"
+            alt=""
+          />
+          <h5 className="text-lg font-bold">{person.name}</h5>
+          <p className="text-sm text-darkGrayishBlue">
+            {person.review}
+          </p>
         </div>
-        </div>
-     </div>
-</>
+      ))}
+    </div>
+
+    <div className="my-16">
+      <a
+        href="#"
+        className="p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight"
+      >
+        Get Started
+      </a>
+    </div>
+  </div>
+</section>
+
   )
 }
 
